cabee3d7fe27cc9c04b65f0513d271cb9fb7e477
Jakub Sokol <sokoljakub14@gmail.com>
2026-04-13 12:32:53 +0200

    FEAT: add Template component with responsive styles and i18n setup

---
+++ client/app/components/Template.vue
@@ -0,0 +1,19 @@
+﻿<script setup lang="ts">
+const { t } = useI18n();
+</script>
+
+<template>
+</template>
+
+<style module lang="scss">
+@use "~/assets/variables" as *;
+
+@media screen and (max-width: $laptopBreakpoint) {
+}
+
+@media screen and (max-width: $tabletBreakpoint) {
+}
+
+@media screen and (max-width: $mobileBreakpoint) {
+}
+</style>
