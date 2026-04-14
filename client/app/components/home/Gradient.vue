672f0d1477bc0037d55eb2ec637b140a26265760
Jakub Sokol <sokoljakub14@gmail.com>
2026-04-14 13:29:42 +0200

    FEAT: add Gradient component and integrate it into Section2

---
+++ client/app/components/home/Gradient.vue
@@ -0,0 +1,31 @@
+﻿<template>
+    <div :class="$style.gradient"></div>
+</template>
+
+<style module lang="scss">
+@use "~/assets/variables" as *;
+
+.gradient {
+    --gradient-height: 700px;
+    
+    top: calc(var(--gradient-height) / -2);
+    position: absolute;
+    width: max(1000px, 101%);
+    left: 50%;
+    transform: translateX(-50%);
+    background: radial-gradient(
+        150% 100% at 50% 100%,
+        #FFF 50%,
+        var(--color-primary) 55%,
+        black 75%,
+        rgba(0, 0, 0, 0.7) 85%,
+        transparent 100%
+    );
+    height: var(--gradient-height);
+    pointer-events: none;
+    
+    @media screen and (max-width: $mobileBreakpoint) {
+        --gradient-height: 600px;
+    }
+}
+</style>
--- client/app/components/home/Section2.vue
+++ client/app/components/home/Section2.vue
@@ -1,11 +1,13 @@
 ﻿<script setup lang="ts">
 
+import Gradient from "~/components/home/Gradient.vue";
+
 const { t } = useI18n();
 </script>
 
 <template>
     <section>
-        <div :class="$style.gradient"></div>
+        <Gradient />
     </section>
 </template>
 
@@ -13,34 +15,10 @@ const { t } = useI18n();
 @use "~/assets/variables" as *;
 
 section {
-    --gradient-height: 700px;
-
     width: 100%;
     min-height: 1500px;
     background-color: white;
     position: relative;
     overflow-x: clip;
-
-    .gradient {
-        top: calc(var(--gradient-height) / -2);
-        position: absolute;
-        width: max(1000px, 100%);
-        left: 50%;
-        transform: translateX(-50%);
-        background: radial-gradient(
-            150% 100% at 50% 100%,
-            #FFF 50%,
-            var(--color-primary) 55%,
-            black 75%,
-            rgba(0, 0, 0, 0.7) 85%,
-            transparent 100%
-        );
-        height: var(--gradient-height);
-        pointer-events: none;
-    }
-
-    @media screen and (max-width: $mobileBreakpoint) {
-        --gradient-height: 600px;
-    }
 }
 </style>
