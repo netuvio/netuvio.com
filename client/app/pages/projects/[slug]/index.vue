173d7a65664a96b400a04ac15c0daec652168311
Jakub Sokol <sokoljakub14@gmail.com>
2026-07-15 00:36:43 +0200

    FEAT: add quick project page

---
+++ client/app/pages/projects/[slug]/index.vue
@@ -0,0 +1,45 @@
+﻿<script setup lang="ts">
+import {useFetch, useRoute} from "#app";
+import type {Project} from "~/lib/types";
+import {computed} from "vue";
+
+const { t, locale } = useI18n();
+
+const route = useRoute();
+
+const { data: project, error } = await useFetch<Project>(`/api/v1/projects/${route.params.slug}`, {
+    query: { locale }
+});
+
+if (error.value || !project.value) {
+    throw createError({
+        statusCode: 404,
+        statusMessage: 'Project Not Found',
+        fatal: true
+    });
+}
+</script>
+
+<template>
+    <div :class="$style.test">
+        <h1>{{ project?.title }}</h1>
+        <p>{{ project?.body }}</p>
+    </div>
+</template>
+
+<style module lang="scss">
+@use "~/assets/variables" as *;
+
+.test {
+    margin: 128px 64px 256px;
+}
+
+@media screen and (max-width: $laptopBreakpoint) {
+}
+
+@media screen and (max-width: $tabletBreakpoint) {
+}
+
+@media screen and (max-width: $mobileBreakpoint) {
+}
+</style>
