1d12e9210325a08cd26521641e429eab5de83c6d
Jakub Sokol <sokoljakub14@gmail.com>
2026-08-25 00:59:19 +0200

    FEAT: add WhoWeAreServiceCard component and integrate into WhoWeAreSection

--- client/app/components/home/WhoWeAreSection.vue
+++ client/app/components/home/WhoWeAreSection.vue
@@ -3,6 +3,7 @@
 import Gradient from "~/components/home/Gradient.vue";
 import {useScroll, useTransform, motion} from "motion-v";
 import Title from "~/components/typography/Title.vue";
+import WhoWeAreServiceCard from "~/components/home/WhoWeAreServiceCard.vue";
 
 const { t } = useI18n();
 
@@ -13,6 +14,32 @@ const { t } = useI18n();
         <div :class="['container', $style.container]">
             <Title>{{ t('whoWeAre.title') }}</Title>
             <h2 v-html="t('whoWeAre.subtitle')"></h2>
+            <section :class="$style.cards">
+                <WhoWeAreServiceCard
+                    title="Lorem ipsum dolor sit amet"
+                    image="/images/who-we-are-card-1.svg"
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </WhoWeAreServiceCard>
+                <WhoWeAreServiceCard
+                    title="Lorem ipsum dolor sit amet"
+                    image="/images/who-we-are-card-1.svg"
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </WhoWeAreServiceCard>
+                <WhoWeAreServiceCard
+                    title="Lorem ipsum dolor sit amet"
+                    image="/images/who-we-are-card-1.svg"
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </WhoWeAreServiceCard>
+                <WhoWeAreServiceCard
+                    title="Lorem ipsum dolor sit amet"
+                    image="/images/who-we-are-card-1.svg"
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </WhoWeAreServiceCard>
+            </section>
         </div>
     </section>
 </template>
@@ -23,47 +50,58 @@ const { t } = useI18n();
 .section {
     padding-top: 180px;
     
-    h2 {
-        font-size: 48px;
-        white-space: pre;
-        line-height: 1.2;
-        position: relative;
-        width: max-content;
-        margin-top: 16px;
-        
-        &::after {
-            content: "";
-            display: block;
-            position: absolute;
-            top: -50px;
-            right: -50px;
-            width: 85px;
-            height: 73px;
-            background-image: url("/images/star-1.svg");
-            background-size: contain;
-            background-repeat: no-repeat;
-            background-position: center;
-        }
-        
-        span {
+    .container {
+        >h2 {
+            font-size: 48px;
+            white-space: pre;
+            line-height: 1.2;
             position: relative;
-            display: inline-block;
+            width: max-content;
+            margin-top: 16px;
 
-            &::before {
+            &::after {
                 content: "";
                 display: block;
                 position: absolute;
-                left: 50%;
-                top: 50%;
-                transform: translate(-50%, -50%);
-                height: 64px;
-                width: 256px;
-                background-image: url("/images/squiggle-3.svg");
+                top: -50px;
+                right: -50px;
+                width: 85px;
+                height: 73px;
+                background-image: url("/images/star-1.svg");
                 background-size: contain;
                 background-repeat: no-repeat;
                 background-position: center;
                 z-index: -1;
             }
+
+            span {
+                position: relative;
+                display: inline-block;
+
+                &::before {
+                    content: "";
+                    display: block;
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    transform: translate(-50%, -50%);
+                    height: 64px;
+                    width: 256px;
+                    background-image: url("/images/squiggle-3.svg");
+                    background-size: contain;
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    z-index: -1;
+                }
+            }
+        }
+        
+        .cards {
+            display: flex;
+            gap: 32px;
+            margin-top: 64px;
+            justify-content: space-between;
+            flex-wrap: wrap;
         }
     }
 }
---
+++ client/app/components/home/WhoWeAreServiceCard.vue
@@ -0,0 +1,165 @@
+﻿<script setup lang="ts">
+const props = defineProps<{
+    title: string;
+    image: string;
+}>();
+</script>
+
+<template>
+    <div :class="$style.card">
+        <section :class="$style.image">
+            <img :src="image" alt="" />
+        </section>
+        <section :class="$style.content">
+            <div>
+                <h2>{{title}}</h2>
+                <div :class="$style.text">
+                    <p><slot /></p>
+                    <img src="/images/arrow.svg" alt="Learn more" />
+                </div>
+            </div>
+        </section>
+        <div :class="$style.decorations">
+            <div :class="$style.dots">
+                <img src="/images/dots.svg" alt="" />
+                <img src="/images/dots.svg" alt="" />
+            </div>
+            <div :class="$style.cylinders">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style module lang="scss">
+@use "~/assets/variables" as *;
+
+.card {
+    width: 350px;
+    height: 500px;
+    border: 4px solid var(--color-background-primary);
+    border-radius: 24px;
+    padding: 32px;
+    position: relative;
+    transition: 200ms all ease-in-out;
+    
+    .image {
+        height: 40%;
+    }
+
+    .content {
+        height: 60%;
+        
+        >div {
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+        }
+        
+        h2 {
+            font-size: 32px;
+            line-height: 1.2;
+        }
+        
+        .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            flex-grow: 1;
+            
+            p {
+                margin-top: 8px;
+                font-size: 18px;
+            }
+            
+            img {
+                width: 24px;
+                height: 24px;
+                padding: 4px;
+                background-color: var(--color-primary);
+            }
+        }
+    }
+    
+    .decorations {
+        position: absolute;
+        inset: 0;
+        pointer-events: none;
+        overflow: hidden;
+        
+        .dots {
+            position: absolute;
+            inset: 0;
+            
+            img {
+                position: absolute;
+                object-fit: contain;
+                
+                &:nth-child(1) {
+                    top: 0;
+                    left: 20%;
+                    width: 160px;
+                    height: 160px;
+                }
+                
+                &:nth-child(2) {
+                    bottom: -10%;
+                    right: 0;
+                    width: 128px;
+                    height: 128px;
+                }
+            }
+        }
+        
+        .cylinders {
+            position: absolute;
+            inset: 0;
+            
+            div {
+                position: absolute;
+                border-radius: 9999px;
+                background-color: var(--color-background-primary);
+                transition: 200ms all ease-in-out;
+
+                &:nth-child(1) {
+                    width: 20px;
+                    height: 128px;
+                    top: 20%;
+                    right: 0;
+                    transform: translateX(50%);
+                }
+
+                &:nth-child(2) {
+                    width: 128px;
+                    height: 20px;
+                    bottom: 0;
+                    right: 15%;
+                    transform: translateY(50%);
+                }
+            }
+        }
+    }
+}
+
+.card:hover {
+    border-color: var(--color-primary);
+    
+    .decorations {
+        .cylinders {
+            div {
+                background-color: var(--color-primary);
+            }
+        }
+    }
+}
+
+@media screen and (max-width: $laptopBreakpoint) {
+}
+
+@media screen and (max-width: $tabletBreakpoint) {
+}
+
+@media screen and (max-width: $mobileBreakpoint) {
+}
+</style>
